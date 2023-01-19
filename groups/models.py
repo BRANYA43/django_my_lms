eacf80e3881bd1e0313bc5a815fbef689fa0c905
Timchenko Bogdan <tbd2504@.gmail.com>
2023-01-19 16:08:34 +0200

    Create model and migrate Group

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    title = models.CharField(max_length=50)
+    start_date = models.DateField()
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        __tablename__ = 'groups'
--- lms/settings.py
+++ lms/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_extensions',
 
     'students.apps.StudentsConfig',
+    'groups.apps.GroupsConfig',
 ]
 
 MIDDLEWARE = [
