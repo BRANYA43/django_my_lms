391e860fb06395df9316a3fba8b70c5fc154f23b
Timchenko Bogdan <tbd2504@.gmail.com>
2023-01-19 16:01:07 +0200

    Add model and migration Student

--- students/models.py
+++ students/models.py
@@ -1,3 +1,16 @@
+from datetime import date
+
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name='First name', db_column='f_name')
+    last_name = models.CharField(max_length=50, verbose_name='Last name', db_column='l_name')
+    birthday = models.DateField(default=date.today)
+    city = models.CharField(max_length=25, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        __tablename__ = 'students'
+
