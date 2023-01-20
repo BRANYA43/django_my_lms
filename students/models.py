76f4d982baee54c1dbd743a0d8fbed7882b9476e
Timchenko Bogdan <tbd2504@.gmail.com>
2023-01-20 17:32:30 +0200

    add column Age

--- students/models.py
+++ students/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=50, verbose_name='First name', db_column='f_name')
     last_name = models.CharField(max_length=50, verbose_name='Last name', db_column='l_name')
+    age = models.PositiveIntegerField()
     birthday = models.DateField(default=date.today)
     city = models.CharField(max_length=25, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
