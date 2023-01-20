09d01063c2f987b2bd7e9cccca278a1e7e2fef95
Timchenko Bogdan <tbd2504@.gmail.com>
2023-01-20 18:34:29 +0200

    Add models

--- students/models.py
+++ students/models.py
@@ -1,17 +1,39 @@
 from datetime import date
 
+from django.core.validators import MinLengthValidator
 from django.db import models
+from faker import Faker
+
+from .validators import validate_email_domain, ValidateEmailDomain
+
+VALID_DOMAINS = ('gmail.com', 'yahoo.com', 'test.com')
 
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=50, verbose_name='First name', db_column='f_name')
+    first_name = models.CharField(max_length=50, verbose_name='First name', db_column='f_name',
+                                  validators=[MinLengthValidator(3)])
     last_name = models.CharField(max_length=50, verbose_name='Last name', db_column='l_name')
     age = models.PositiveIntegerField()
     birthday = models.DateField(default=date.today)
     city = models.CharField(max_length=25, null=True, blank=True)
+    email = models.EmailField(validators=[ValidateEmailDomain(*VALID_DOMAINS)])
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
 
     class Meta:
         __tablename__ = 'students'
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.birthday} {self.age} {self.email}'
+
+    @classmethod
+    def generate_fake_data(cls, count: int):
+        faker = Faker()
+        for _ in range(count):
+            student = cls()
+            student.first_name = faker.first_name()
+            student.last_name = faker.last_name()
+            student.email = f'{student.last_name}@{faker.random.choice(VALID_DOMAINS)}'
+            student.birthday = faker.date_between(start_date='-65y', end_date='-18y')
+            student.age = faker.random_int(min=18, max=65)
+            student.save()
---
+++ students/validators.py
@@ -0,0 +1,22 @@
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+
+VALID_DOMAINS = ('gmail.com',
+                 'yahoo.com',)
+
+
+def validate_email_domain(value: str):
+    domain = value.split('@')[1]
+    if domain not in VALID_DOMAINS:
+        raise ValidationError(f'Email domain {domain} is not correct.')
+
+
+@deconstructible
+class ValidateEmailDomain:
+    def __init__(self, *domains: tuple):
+        self.domains = tuple(domains) if domains else VALID_DOMAINS
+
+    def __call__(self, *args, **kwargs):
+        domain = args[0].split('@')[1]
+        if domain not in self.domains:
+            raise ValidationError(f'This email domain {domain} is not correct.')
