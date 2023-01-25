b439767997c73ca14f7bc07f15be91713b951fad
Timchenko Bogdan <tbd2504@.gmail.com>
2023-01-25 19:43:04 +0200

    Create template and view for teacher list

--- teachers/views.py
+++ teachers/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponseRedirect
 
-# Create your views here.
+from .models import Teacher
+
+
+def get_render_list(request: HttpRequest):
+    teachers = Teacher.objects.all().order_by('first_name')
+    return render(request=request,
+                  template_name='teachers/list.html',
+                  context={'title': 'List of Teachers', 'teachers': teachers})
