65e67fee2106f97a93838a34cb91bdfab070f602
Timchenko Bogdan <tbd2504@.gmail.com>
2023-01-27 19:01:57 +0200

    merge

--- lms/urls.py
+++ lms/urls.py
@@ -15,17 +15,14 @@ urlpatterns = [
     path('students/create/', create_student_view),
     path('students/update/<int:pk>/', update_student),
     path('students/detail/<int:pk>/', detail_student),
-<<<<<<< HEAD
     path('groups/', get_render_list),
     path('groups/create/', get_render_create),
     path('groups/update/<int:pk>/', get_render_update),
-    path('groups/detail/<int:pk>/', get_render_detail)
-=======
+    path('groups/detail/<int:pk>/', get_render_detail),
     path('teachers/', get_render_list),
     path('teachers/create/', get_render_create),
     path('teachers/update/<int:pk>/', get_render_update),
-    path('teachers/detail/<int:pk>/', get_render_detail)
->>>>>>> teachers
+    path('teachers/detail/<int:pk>/', get_render_detail),
 
     # path('test/route/param/', view_without_param),          # test/route/param/
     # path(r'test/route/<str:value>/', view_with_param),       # test/route/df;lkjhrlkjgf's/
