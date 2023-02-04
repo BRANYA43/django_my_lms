e80d47ea8aa62b72e58686016442779b92938258
Timchenko Bogdan <tbd2504@.gmail.com>
2023-02-04 20:01:35 +0200

    Install debug-toolbar

--- lms/settings.py
+++ lms/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap5",
     'django_filters',
+    "debug_toolbar",
 
     'students.apps.StudentsConfig',
     'groups.apps.GroupsConfig',
@@ -58,6 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'lms.urls'
@@ -139,3 +142,9 @@ if DEBUG:
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
