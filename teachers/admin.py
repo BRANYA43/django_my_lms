03f67227546168033980afdc75d26f18e568df70
Timchenko Bogdan <tbd2504@.gmail.com>
2023-02-27 14:20:27 +0200

    Add teacher in admin page

--- teachers/admin.py
+++ teachers/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Teacher
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    list_display_links = list_display
+    list_per_page = 15
+    search_fields = ('first_name', 'last_name')
+    fieldsets = (
+        ('Personal info', {'fields': (('first_name', 'last_name'),)}),
+        ('Born', {'fields': (('birthday', 'get_age'),)}),
+        ('Contact', {'fields': (('email', 'city', 'phone'),)}),
+        (' ', {'fields': ('salary',)},),
+    )
+
+    def get_age(self, instance):
+        return f'{instance.get_age()} year(s)'
+
+    get_age.short_description = 'age'
+    readonly_fields = (
+        'get_age',
+    )
